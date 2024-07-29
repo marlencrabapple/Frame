b0c84d90a5f5eaebccc065070b5631f41e22e2c7
Ian P Bradley <ian@hikki.tech>
2024-07-28 23:49:17 -0700

    Misc

--- lib/Plack/Handler/Frame/Server.pm
+++ lib/Plack/Handler/Frame/Server.pm
@@ -6,11 +6,13 @@ class Plack::Handler::Frame::Server :does(Frame::Base);
 use utf8;
 use v5.36;
 
+use List::Util;
+use Data::Dumper;
 use Frame::Server;
 use IO::Async::Loop;
 use Parallel::Prefork;
 use Server::Starter ();
-use List::AllUtils qw(any first);
+use List::AllUtils qw(any first notall);
 
 use constant SIGRERE => qr/^(TERM|USR1)$/;
 
@@ -22,7 +24,11 @@ field $read_timeout :reader :param = 300;
 field $inactivity_timeout :reader :param = 30;
 field $max_workers :param = 10;
 field $queue_size :param = 10;
+
 field $ssl :param = undef;
+field $ssl_server :param = undef;
+field $ssl_cert_file :param = undef;
+field $ssl_key_file :param = undef;
 
 field @listen;
 field %ssl_args;
@@ -33,7 +39,7 @@ field $server_ready;
 ADJUSTPARAMS ($params) {
   # TODO: Server::Starter stuff
   # ...
-  
+
   $server_ready = $$params{server_ready}
     if $$params{server_ready} && ref $$params{server_ready} eq 'CODE';
 
@@ -56,13 +62,45 @@ ADJUSTPARAMS ($params) {
   $pm_args{err_respawn_interval} = $$params{err_respawn_interval}
     if $$params{err_respawn_interval};
 
-  if($ssl || any { /^ssl_/ } keys %$params) {
+  if($ssl || any { /^ssl(_)?/ } keys %$params) {
     require IO::Async::SSL;
-    $ssl_args{extensions} = ['SSL'];
-    %ssl_args = map { $_ =~ s/^ssl/SSL/r, $$params{$_} } grep { /^ssl_/ } keys %$params
+
+    %ssl_args = (
+      # %ssl_args,
+      # map { $_ =~ s/^ssl/SSL/r, $$params{$_} }
+      #   grep { /^ssl(?:_)?/ } keys %$params,
+      SSL_cert_file => $ssl_cert_file,
+      SSL_key_file => $ssl_key_file,
+      extensions => ['SSL'],
+      SSL => 1,
+      SSL_server => 1 
+    );
   }
 
   $loop = IO::Async::Loop->new
+}
+
+method BUILDARGS :common (@args) {
+  my @_args= ();
+
+  use constant PREFIX => qr/^-{1,2}/;
+  use constant FLAG => qr/^(?:\-{1,2})?([a-z_\-]+|[^0-1]{1})$/;
+
+  for (my $i = 0; $i < scalar @args; $i++) {
+    $args[$i] =~ s/${\PREFIX}// if $args[$i];
+    $args[$i + 1] =~ s/${\PREFIX}// if $args[$i + 1];
+
+    if ($args[$i + 1] && $args[$i + 1] =~ FLAG) {
+      push @_args, $args[$i], 1;
+      next
+    }
+    else {
+      push @_args, $args[$i], $args[$i + 1];
+      $i++
+    }
+  }
+
+  @_args
 }
 
 method run ($app) {
