e9ba10bce17738782910cfd7f37980b5d9eb5a35
Ian P Bradley <ian.bradley@studiocrabapple.com>
2025-03-11 12:59:05 -0700

    Start hacking on on some light db schema versioning and other things typical of the bare minimum that fit the bare minimum criteria for the problem scope

--- lib/Frame/Db.pm
+++ lib/Frame/Db.pm
@@ -4,38 +4,34 @@ package Frame::Db;
 role Frame::Db :does(Frame::Base);
 
 use utf8;
-use v5.36;
+use v5.40;
 # use autodie;
 
 use Data::Printer;
 use SQL::Abstract;
 use Syntax::Keyword::Try;
 
+field $sqla = SQL::Abstract->new;
+
 APPLY ($mop) {
   try {
-    p $ENV{config};
-    
-    my ($driver) = $ENV{config}->{db}{source} =~ /dbi:([^:]+):.+/;
-      my $fn = "Frame/Db/$driver.pm";
-    require "$fn";
-    $mop->add_role("Frame::Db::$driver");
+    #my ($driver) = $ENV{config}->{db}{source} =~ /dbi:([^:]+):.+/;
+    #my $fn = "Frame/Db/$driver.pm";
+    #require "$fn";
+    #$mop->add_role("Frame::Db::$driver");
   }
   catch ($err) {
     p $err
   }
 }
 
-
-
 ADJUST {
   my $mop = Object::Pad::MOP::Class->for_caller;
   my ($driver) = $self->config->{db}{source} =~ /dbi:([^:]+):.+/;
-      my $fn = "Frame/Db/$driver.pm";
-    require "$fn";
+  my $fn = "Frame/Db/$driver.pm";
+  require "$fn";
   $mop->add_role("Frame::Db::$driver");
 }
-
-field $sqla = SQL::Abstract->new;
 
 method _dbh {
   DBI->connect_cached($self->app->config->{db}->@{qw/source username auth attr/})
@@ -43,4 +39,3 @@ method _dbh {
 
 #method dbh :required;
 
-1
