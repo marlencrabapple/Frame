72cb8c92eb66502dd3dd7782b5cb6ad73dd424c5
Ian P Bradley <ian.bradley@studiocrabapple.com>
2025-05-06 01:06:14 -0700

    Frame::PW: ...

--- lib/Frame/PW.pm
+++ lib/Frame/PW.pm
@@ -1,20 +1,42 @@
 use Object::Pad qw(:experimental(:all));
 
 package Frame::PW;
-role Frame::PW : does(Frame::Base);
+role Frame::PW;
 
 use utf8;
 use v5.40;
 
 use Carp;
+use POSIX;
+use Const::Fast;
+use Net::SSLeay;
 use Crypt::Argon2 qw/argon2id_pass argon2_verify/;
+
+const our @UNAME = POSIX::uname(); 
+const our $NPROC = ``;
+
+APPLY {
+  die "Required binary pwgen not found in PATH." unless system(qw(which pwgen));
+  dmsg @UNAME if $ENV{DEBUG};
+}
 
 field %users : inheritable = ();
 
-method add_pass ( $user, $pass ) {
-    my $salt     = get_random(32);
-    my $hashpass = argon2id_pass( $pass, $salt, 3, '64M', `echo $(nproc)`, 32 );
-    store_pass( $user, $hashpass );
+method add_pass ($user, $pass) {
+  my $salt = get_random(32);
+  my $hashpass = argon2id_pass($pass, $salt, 3, '64M', `echo $(nproc)`, 32);
+  store_pass($user, $hashpass)
+}
+
+method hashpass :common ($pw = `pwgen -s 32`) {
+  die "Error generating password hash/no password provided: $?" unless $pw;
+
+  my $salt;
+  my $rv = Net::SSLeay::RAND_bytes($salt, 32);
+
+  die "Error generating random salt: $rv" unless $rv == 1 && defined $salt;
+
+  argon2id_pass($pw, $salt, '4', '32M', $NPROC, 16)
 }
 
 method check_pass ( $user, $pass ) {
