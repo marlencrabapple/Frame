70add1ab58be4419d76769d603a3c9bd3e1966cf
Ian P Bradley <ian.bradley@studiocrabapple.com>
2025-06-04 00:15:48 -0700

    Clean build artifacts in repo, refreshed LICENSE and Build.PL

--- blib/lib/Frame/Controller/Default.pm
+++
@@ -1,9 +0,0 @@
-use Object::Pad;
-
-package Frame::Controller::Default;
-class Frame::Controller::Default :does(Frame::Controller);
-
-use utf8;
-use v5.36;
-
-1
--- lib/Frame/Config.pm
+++ lib/Frame/Config.pm
@@ -1,7 +1,7 @@
 use Object::Pad qw(:experimental(:all));
 
 package Frame::Config;
-role Frame::Config :does(Frame::Base);
+role Frame::Config : does(Frame::Base);
 
 use utf8;
 use v5.40;
@@ -15,39 +15,38 @@ use Const::Fast;
 our $_config_default;
 
 try {
-  my $error;
-  
-  ($_config_default, $error) = from_toml(
-    path($ENV{FRAME_DEFAULT_CONFIG_FILE}
-      // 'config-default.toml')
-    ->slurp_utf8);
+    my $error;
 
-  if ($error) {
-    die $error
-  }
+    ( $_config_default, $error ) = from_toml(
+        path( $ENV{FRAME_DEFAULT_CONFIG_FILE} // 'config-default.toml' )
+          ->slurp_utf8 );
+
+    if ($error) {
+        die $error;
+    }
 }
 catch ($e) {
-  $_config_default = { charset => 'utf8' };
-  warn Dumper($e)
+    $_config_default = { charset => 'utf8' };
+    Frame::Base::dmsg($e)
 }
 
-const our $config_default => { %$_config_default };
+const our $config_default => {%$_config_default};
 
-field $config_in :param(config) = [ 'config-default.toml', 'config.toml' ];
-field $config = { %$config_default };
+field $config_in : param(config) = [ 'config-default.toml', 'config.toml' ];
+field $config = {%$config_default};
 
 APPLY {
 
-}
-
-ADJUST {
-  my $_config = {};
-
-  foreach my $in (@$config_in) {
-    my $_config_in = from_toml(path($in)->slurp_utf8);
-    $_config = { %$_config, %$_config_in }
   }
 
-  const our $run_config = { %$_config };
-  $config = $run_config
+  ADJUST {
+    my $_config = {};
+
+    foreach my $in (@$config_in) {
+        my $_config_in = from_toml( path($in)->slurp_utf8 );
+        $_config = { %$_config, %$_config_in };
+    }
+
+    const our $run_config = {%$_config};
+    $config = $run_config
 }
